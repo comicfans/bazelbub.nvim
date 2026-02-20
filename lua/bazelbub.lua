ab601cd9ac02ff3d6c3e1ae255bf47463b23aa8b
comicfans <comcifans44@gmail.com>
2026-02-20 10:09:24 +0100

    temp

--- lua/bazelbub.lua
+++ lua/bazelbub.lua
@@ -31,6 +31,89 @@ function highlight_passed(bufid, hlname, match_string, hlcolor)
   end
 end
 
+function file_exists(p)
+    local f = io.open(p, "r")
+    if f then f:close() return true end
+    return false
+end
+
+
+function find_bazel_root(filepath)
+    -- Normalize separators
+    local path = filepath:gsub("\\", "/")
+    
+    -- Split path into segments
+    local segments = {}
+    for segment in path:gmatch("[^/]+") do
+        table.insert(segments, segment)
+    end
+
+    local workspace_root_idx = -1
+    local root_files = {"WORKSPACE", "WORKSPACE.bazel", "MODULE.bazel"}
+
+    -- 1. Walk up to find the root
+    for i = #segments, 1, -1 do
+        -- Reconstruct path up to current segment
+        local current_path = "/" .. table.concat(segments, "/", 1, i) .. "/"
+
+        local found = false
+        for j = 1, #root_files do
+            -- FIXED: Use 'j' to index root_files, not 'i'
+            if file_exists(current_path .. root_files[j]) then
+                workspace_root_idx = i
+                found = true
+                break
+            end
+        end
+
+        if found then break end
+    end
+
+    if workspace_root_idx == -1 then 
+        return nil, "Bazel root not found" 
+    end
+
+    -- FIXED: Use workspace_root_idx and remove the assignment '='
+    return "/" .. table.concat(segments, "/", 1, workspace_root_idx)
+end
+
+function bazel_path(workspace_root, filepath)
+    local p = Path:new(filepath)
+    local root = Path:new(workspace_root):absolute()
+    
+    local build_dir = nil
+    local current = p:parent()
+
+    -- 1. Walk up to find the nearest BUILD/BUILD.bazel file
+    while true do
+        if Path:new(current, "BUILD"):exists() or Path:new(current, "BUILD.bazel"):exists() then
+            build_dir = current
+            break
+        end
+        
+        -- Stop if we've reached the workspace root or filesystem root
+        if current:absolute() == root or current:absolute() == "/" then
+            break
+        end
+        current = current:parent()
+    end
+
+    -- Fallback: If no BUILD file found, we can't construct a valid target
+    if not build_dir then return nil end
+
+    -- 2. Construct the Package part (from root to build_dir)
+    -- make_relative returns a string
+    local pkg = Path:new(build_dir):make_relative(root)
+    if pkg == "." then pkg = "" end -- Handle BUILD file at workspace root
+
+    -- 3. Construct the Target part (from build_dir to the file)
+    local target = Path:new(filepath):make_relative(build_dir:absolute())
+
+    -- 4. Combine into Bazel format: //package:target
+    return string.format("//%s:%s", pkg, target)
+end
+
+
 function showFloatWindow(title, content)
   local float = Float.percentage_range_window(0.6, 0.3, { winblend = 5 }, {
     title = title,
@@ -55,21 +138,37 @@ end
 
 --- Run the given command and return the output and exit code
 -- @param the command to run
-function runCommand(executeCommand)
-    executeCommand = string.format("%s; echo $?", executeCommand)
-    local handle = io.popen(executeCommand)
+function runCommand(dir, executeCommand)
 
-    local output = {}
-    for line in handle:lines() do
-        table.insert(output, line)
-    end
 
+    local out_tmp = os.tmpname()
+    local err_tmp = os.tmpname()
+
+    local fullCommand = string.format("cd \"%s\";(%s) > \"%s\" 2> \"%s\"; echo $?", 
+                                      dir ,executeCommand, out_tmp, err_tmp)
+    local handle = io.popen(fullCommand)
+    local lastLine = handle:read("*a")
     -- Close the handle
     handle:close()
 
+    local read_and_delete = function(filename)
+        local f = io.open(filename, "r")
+        local content = f:read("*a")
+        f:close()
+        os.remove(filename)
+        return content
+    end
+
+    local stdout = read_and_delete(out_tmp)
+    local stderr = read_and_delete(err_tmp)
+
     -- Get the exit code from the last line of the output
-    local exitCode = tonumber(table.remove(output))
-    return output, exitCode
+    local exitCode = tonumber(lastLine)
+    return exitCode, stdout, stderr
+end
+
+function buildError(head, stdout, stderr)
+    return head .. "\nstdout:\n" .. stdout .. "stderr:\n" .. stderr
 end
 
 local M = {}
@@ -81,33 +180,31 @@ function M.onInsertEnter()
   vim.b.whitespace_lastline = curline
 end
 
+
+
 function M.runGazelle()
-  local result, exit = runCommand("bazel run //:gazelle 2>&1")
+
+  -- always run from current file dir (since vim cmd might not always under workspace)
+  local dir = plenary.path:new(vim.api.nvim_buf_get_name(0)).parent()
+  local exit, stdout, stderr = runCommand(dir, "bazel run //:gazelle")
   local dBuffer = ""
   local title = "Gazelle ran successfully"
   if exit > 0 then
-    dBuffer = "bazel run //:gazelle failed!\n"
+    dBuffer = buildError("bazel run //:gazelle failed!", stdout, stderr)
     title = "Gazelle failed"
-  end
-
-  for _, v in pairs(result) do
-    dBuffer = string.format("%s%s\n", dBuffer, v)
   end
 
   showFloatWindow(title, vim.split(dBuffer, "\n"))
 end
 
 function M.runGazelleUpdateRepos()
-  local result, exit = runCommand("bazel run //:gazelle-update-repos 2>&1")
+  local dir = plenary.path:new(vim.api.nvim_buf_get_name(0)).parent()
+  local exit, stdout, stderr = runCommand(dir, "bazel run //:gazelle-update-repos")
   local dBuffer = ""
   local title = "Gazelle update repos ran successfully"
   if exit > 0 then
-    dBuffer = "bazel run //:gazelle-update-repos failed!\n"
+    dBuffer = buildError("bazel run //:gazelle-update-repos failed!", stdout, stderr)
     title = "Gazelle update repos failed"
-  end
-
-  for _, v in pairs(result) do
-    dBuffer = string.format("%s%s\n", dBuffer, v)
   end
 
   showFloatWindow(title, vim.split(dBuffer, "\n"))
@@ -115,17 +212,14 @@ end
 
 -- Get the test targets for the file in the current buffer.
 function M.getTestTargets()
+  local dir = plenary.path:new(vim.api.nvim_buf_get_name(0)).parent()
   local fpa_rel = plenary.path:new(vim.api.nvim_buf_get_name(0)):make_relative()
-  local result, exit = runCommand(string.format("bazel query 'kind(test, rdeps(//..., %s))' --keep_going", fpa_rel))
+  local exit, stdout, stderr = runCommand(dir, string.format("bazel query 'kind(test, rdeps(//..., %s))' --keep_going", fpa_rel))
   local dBuffer = ""
   local title = "Bazel targets"
   if exit > 0 then
-    dBuffer = "Bazel failed\n"
+    dBuffer = buildError("Bazel failed", stdout, stderr)
     title = "Bazel run failed"
-  end
-
-  for _, v in pairs(result) do
-    dBuffer = string.format("%s%s\n", dBuffer, v)
   end
 
   showFloatWindow(title, vim.split(dBuffer, "\n"))
@@ -133,17 +227,14 @@ end
 
 -- Get the build targets for the file in the current buffer.
 function M.getBuildTargets()
+  local dir = plenary.path:new(vim.api.nvim_buf_get_name(0)).parent()
   local fpa_rel = plenary.path:new(vim.api.nvim_buf_get_name(0)):make_relative()
-  local result, exit = runCommand(string.format("bazel query 'rdeps(//..., %s)' --keep_going", fpa_rel))
+  local exit, stdout, stderr = runCommand(dir, string.format("bazel query 'rdeps(//..., %s)' --keep_going", fpa_rel))
   local dBuffer = ""
   local title = "Bazel targets"
   if exit > 0 then
-    dBuffer = "Bazel failed\n"
+    dBuffer = buildError("Bazel failed", stdout, stderr)
     title = "Bazel run failed"
-  end
-
-  for _, v in pairs(result) do
-    dBuffer = string.format("%s%s\n", dBuffer, v)
   end
 
   showFloatWindow(title, vim.split(dBuffer, "\n"))
@@ -151,10 +242,11 @@ end
 
 -- Run bazel test on the targets for the current file.
 function M.runTestTargets()
+  local dir = plenary.path:new(vim.api.nvim_buf_get_name(0)).parent()
   local fpa_rel = plenary.path:new(vim.api.nvim_buf_get_name(0)):make_relative()
-  local targets, exit = runCommand(string.format("bazel query 'kind(test, rdeps(//..., %s))' --keep_going", fpa_rel))
+  local exit, targets, stderr  = runCommand(dir, string.format("bazel query 'kind(test, rdeps(//..., %s))' --keep_going", fpa_rel))
   if exit > 0 then
-    log.error("Bazel failed")
+    log.error(buildError("Bazel failed",  targets , stderr))
     return
   end
 
@@ -184,12 +276,16 @@ function M.runTestTargets()
   vim.system(cmd, { text = true }, callback)
 end
 
+
+
 -- Build all dependencies for the current file.
 function M.buildTargets()
+
+  local dir = plenary.path:new(vim.api.nvim_buf_get_name(0)).parent()
   local fpa_rel = plenary.path:new(vim.api.nvim_buf_get_name(0)):make_relative()
-  local targets, exit = runCommand(string.format("bazel query 'rdeps(//..., %s)' --keep_going", fpa_rel))
+  local exit,targets,stderr  = runCommand(dir,string.format("bazel query 'rdeps(//..., %s)' --keep_going", file_bazel_path))
   if exit > 0 then
-    log.error("Bazel failed")
+    log.error(buildError("Bazel failed", targets, stderr))
     return
   end
 
@@ -211,7 +307,7 @@ function M.buildTargets()
   cmd[index] = 'build'
   index = index + 1
 
-  for _, v in pairs(targets) do
+  for _, v in ipairs(targets) do
     cmd[index] = v
     index = index + 1
   end
